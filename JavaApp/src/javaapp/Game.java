9389cd261f9b33214c6a2f4e01005a4bc880123f
markjameskerr <35890146+markjameskerr@users.noreply.github.com>
2018-02-05 00:35:42 +0000

    changes to score - using code already there
    no need to create new instance of player as we already have it with allPlayers array

--- JavaApp/src/javaapp/Game.java
+++ JavaApp/src/javaapp/Game.java
@@ -30,8 +30,6 @@ int player2 = 0;
     
     public void playGame(){
         
-        Player p1 = new Player();
-        Player p2 = new Player();
         while(userOption != 3){
             Scanner s = new Scanner(System.in);
             System.out.println("Please choose who won the frame: " + "\n" + "1 = " + allPlayers[0].getName() + "\n" + "2 = " + allPlayers[1].getName()  + "\n" + "3 = Exit Game");
@@ -44,7 +42,7 @@ int player2 = 0;
 			System.out.println(allPlayers[0].getName() + " Won the frame");
                         player1 = player1 + 1;
                         System.out.println(player1);
-                        p1.setFramesWon(player1);
+                        allPlayers[0].setFramesWon(player1);
                         //score[0] = player1;
 			break;
 		}
@@ -52,27 +50,20 @@ int player2 = 0;
 			System.out.println(allPlayers[1].getName() + " Won the frame");
                         player2 = player2 + 1;
                         System.out.println(player2);
-                        p2.setFramesWon(player2);
+                        allPlayers[1].setFramesWon(player2);
                         //score[1] = player2;
 			break;
 		}
 		case 3:{
 			System.out.println("Game Ended");
-                        System.out.println(p1.getFramesWon() + " Frame(s) won by " + allPlayers[0].getName());
-                        System.out.println(p2.getFramesWon() + " Frame(s) won by " + allPlayers[1].getName());
+                        System.out.println(allPlayers[0].getFramesWon() + " Frame(s) won by " + allPlayers[0].getName());
+                        System.out.println(allPlayers[1].getFramesWon() + " Frame(s) won by " + allPlayers[1].getName());
 			break;
         }
         }
         }
     }
     
-    /*public void finalScore(){
-         for (int i = 0; i < allPlayers.length; i ++){
-            //allPlayers[i].getFramesWon();
-            System.out.println(allPlayers[i].getFramesWon() + " Frames won by " + allPlayers[i].getName());
-         }
-    }*/
-
     public Game(int gamemode){
         
         this.gamemode = gamemode;
@@ -85,7 +76,7 @@ int player2 = 0;
         for (int i = 0; i < allPlayers.length; i ++)
         {
             System.out.println("Please enter your name: ");
-            allPlayers[i] = new Player();
+            allPlayers[i] = new Player(); // array storing instances of Player class
             allPlayers[i].setName(s.nextLine());
             System.out.println(allPlayers[i].getName());
 
