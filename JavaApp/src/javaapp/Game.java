5e1106ed8258ba9c0513939a258c4e1e926a458b
markjameskerr <35890146+markjameskerr@users.noreply.github.com>
2018-02-21 19:28:27 +0000

    changed to arraylist
    Arraylist working partially but need to fix the for loop for arraylist size and fix the gamemode being passed in.

--- JavaApp/src/javaapp/Game.java
+++ JavaApp/src/javaapp/Game.java
@@ -5,6 +5,8 @@
  */
 package javaapp;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -23,31 +25,34 @@ int player2 = 0;
         return gamemode;
     }
 
-    private Player[] allPlayers;
-    
+    //private Player[] allPlayers;
     //private int score[] = new int[2];
+    List <Player> allPlayers = new ArrayList <>();
     
         public Game(int gamemode){
+            
         
         this.gamemode = gamemode;
         Scanner s = new Scanner(System.in);
         
         if(gamemode == 1){
-            allPlayers = new Player[2];
+           
         }
         else if(gamemode == 2){
-            allPlayers = new Player[2];
+            
         }
         
-        for (int i = 0; i < allPlayers.length; i ++)
+        for (int i = 0; i < 2; i ++)
         {
             System.out.println("Please enter your name: ");
-            allPlayers[i] = new Player(); // array storing instances of Player class
-            allPlayers[i].setName(s.nextLine());
-            System.out.println(allPlayers[i].getName());
+            //allPlayers[i] = new Player(); // array storing instances of Player class
+            Player player = new Player();
+            player.setName(s.nextLine());
+            allPlayers.add(i, player);
+            System.out.println(allPlayers.get(i).getName());
         }
 }
-        
+       
     public void setupGame(){
             
     }
@@ -57,32 +62,32 @@ int player2 = 0;
         
         while(userOption != 3){
             Scanner s = new Scanner(System.in);
-            System.out.println("Please choose who won the frame: " + "\n" + "1 = " + allPlayers[0].getName() + "\n" + "2 = " + allPlayers[1].getName()  + "\n" + "3 = Exit Game");
+            System.out.println("Please choose who won the frame: " + "\n" + "1 = " + allPlayers.get(0).getName() + "\n" + "2 = " + allPlayers.get(1).getName()  + "\n" + "3 = Exit Game");
             
             userOption = s.nextInt();
             
             switch(userOption){
                 
 		case 1:{
-			System.out.println(allPlayers[0].getName() + " Won the frame");
+			System.out.println(allPlayers.get(0).getName() + " Won the frame");
                         player1 = player1 + 1;
                         System.out.println(player1);
-                        allPlayers[0].setFramesWon(player1);
+                        allPlayers.get(0).setFramesWon(player1);
                         //score[0] = player1;
 			break;
 		}
 		case 2:{
-			System.out.println(allPlayers[1].getName() + " Won the frame");
+			System.out.println(allPlayers.get(1).getName() + " Won the frame");
                         player2 = player2 + 1;
                         System.out.println(player2);
-                        allPlayers[1].setFramesWon(player2);
+                        allPlayers.get(1).setFramesWon(player2);
                         //score[1] = player2;
 			break;
 		}
 		case 3:{
 			System.out.println("Game Ended");
-                        System.out.println(allPlayers[0].getFramesWon() + " Frame(s) won by " + allPlayers[0].getName());
-                        System.out.println(allPlayers[1].getFramesWon() + " Frame(s) won by " + allPlayers[1].getName());
+                        System.out.println(allPlayers.get(0).getFramesWon() + " Frame(s) won by " + allPlayers.get(0).getName());
+                        System.out.println(allPlayers.get(1).getFramesWon() + " Frame(s) won by " + allPlayers.get(1).getName());
 			break;
                         }
                 
